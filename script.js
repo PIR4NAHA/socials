8e19dd10685b5f3d6c691e9ce2c022f2cecf4ffb
PIR44NHA <thomas.keeler@ymail.com>
2022-05-26 04:06:40 +0100

    Add click-to-flip functionality for mobile devices

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-const card = document.getElementById('card');
-var counter = 0;
+const getDeviceType = () => {
+  const ua = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "tablet";
+  }
+  if (
+    /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+      ua
+    )
+  ) {
+    return "mobile";
+  }
+  return "desktop";
+};
 
-card.addEventListener('click', event => {
-    if (counter % 2 == 1) {
-        card.style.transform = "perspective(600px) rotateY(180deg)";
-        console.log("flipping 180")
-    } else {
-        card.style.transform = "perspective(600px) rotateY(-180deg)";
-        console.log("flipping -180")
-    } 
-});
+if (getDeviceType() == "mobile") {
+  var front = document.getElementById('front');
+  var back = document.getElementById('back');
+
+  front.addEventListener('click', event => {
+    front.style.transform = "perspective(600px) rotateY(-180deg)";
+    back.style.transform = "perspective(600px) rotateY(0deg)"
+  });
+
+  back.addEventListener('click', event => {
+    back.style.transform = "perspective(600px) rotateY(180deg)"
+    front.style.transform = "perspective(600px) rotateY(0deg)"
+  });
+}
