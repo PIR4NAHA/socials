da810d8e936a8c253817cfa2efcd64494ca81515
PIR44NHA <thomas.keeler@ymail.com>
2022-05-26 01:44:49 +0100

    added perspective

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const card = document.getElementById('card');
+var counter = 0;
 
 button.addEventListener('click', event => {
-  card.style.transform = "rotateY(180deg)"
+    if (counter % 2 == 1) {
+        card.style.transform = "perspective(600px) rotateY(180deg)";
+        console.log("flipping 180")
+    } else {
+        card.style.transform = "perspective(600px) rotateY(-180deg)";
+        console.log("flipping -180")
+    } 
 });
