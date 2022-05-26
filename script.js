97fcbbb28e7f9e5cc78766d64150eb097bca87c3
PIR44NHA <thomas.keeler@ymail.com>
2022-05-26 01:47:54 +0100

    changed event listener from button to card

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const card = document.getElementById('card');
 var counter = 0;
 
-button.addEventListener('click', event => {
+card.addEventListener('click', event => {
     if (counter % 2 == 1) {
         card.style.transform = "perspective(600px) rotateY(180deg)";
         console.log("flipping 180")
